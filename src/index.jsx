e555cfb11fb74175a76971fca63e93151f21bf4f
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-12 20:15:22 +0200

    importing main-view inside index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MainView from './components/main-view/main-view';
 
 //import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
@@ -8,9 +9,7 @@ import './index.scss';
 class MooViesApplication extends React.Component {
     render() {
         return (
-            <div className="moovies"> 
-                <div>Good Morning</div>
-            </div>
+            <MainView />
         ); 
     }
 }
