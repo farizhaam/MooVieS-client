f05ba258c1cb13734b1599932af783f23471ab34
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-14 17:07:12 +0200

    importing axios and setting movies to null

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+
 import {MovieCard} from '../movie-card/movie-card';
 import {MovieView} from '../movie-view/movie-view';
 
@@ -6,25 +8,7 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state={
-            movies: [
-                { 
-                    _id: 1, Title: 'Inception', 
-                    Description: 'The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets. He is offered a chance to have his criminal history erased as payment for the implantation of another person\'s idea into a target\'s subconscious.', 
-                    ImagePath: '...'
-                },
-                { 
-                    _id: 2, 
-                    Title: 'The Shawshank Redemption', 
-                    Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency. Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit.', 
-                    ImagePath: '...'
-                },
-                { 
-                    _id: 3, 
-                    Title: 'Gladiator', 
-                    Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery. Maximus is a powerful Roman general, loved by the people and the aging Emperor, Marcus Aurelius. Maximus is then relegated to fighting to the death in the gladiator arenas.', 
-                    ImagePath: '...'
-                }
-            ],
+            movies: [],
             selectedMovie: null
         };
     }
