166396911e6190cd375692ec8feccf09ae6fc333
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-21 18:19:14 +0200

    enveloping LoginView inside main page, adding RegistrationView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -81,12 +81,7 @@ export class MainView extends React.Component {
     render(){
         const {movies, user} = this.state;
 
-        //rendering LoginView if there's no user, if yes, passing user details as prop to LoginView
-        if (!user) return <Row>
-            <Col>
-                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-            </Col>
-        </Row> 
+
 
         //before the movies loaded
         if (movies.length === 0) return <div className="main-view" />;
@@ -98,23 +93,50 @@ export class MainView extends React.Component {
             //adding MovieCard and MovieView inside Router
             <Router> 
                 <Row className="main-view justify-content-md-center">
+                    
+                    {/* route to MovieCard */}
                     <Route exact path="/" render={() =>{
+                        
+                        //rendering LoginView if there's no user, if yes, passing user details as prop to LoginView
+                        if (!user) return<Col>
+                            <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+                        </Col>
+
+                        //rendering movieCard is login is successful
                         return movies.map(m => (
                             <Col md={3} key={m._id}>
                                 <MovieCard movie={m} />
                             </Col>
                         ))
                     }} />
-                    <Route path="/movies/:movieId" render={({match}) => {
+                    
+                    {/* route to RegistrationView */}
+                    <Route exact path="/register" render={() =>{
+                        return <Col>
+                            <RegistrationView />
+                        </Col>
+                    }}/>
+
+                    {/* route to MovieView */}
+                    <Route path="/movies/:movieId" render={({match, history}) => {
                         return <Col md={8}>
-                            <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+                            <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
                         </Col>
                     }} />
 
-                    <Route path="/directors/:name" render={({match}) => {
+                    {/* route for DirectorView) */}
+                    <Route path="/directors/:name" render={({match, history}) => {
                         if (movies.length === 0) return <div className="main-view" />
                         return <Col>
-                            <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} />
+                            <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+                        </Col>
+                    }} />
+
+                    {/* route for GenreView) */}
+                    <Route path="/genres/:name" render={({match, history}) => {
+                        if (movies.length === 0) return <div className="main-view" />
+                        return <Col>
+                            <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
                         </Col>
                     }} />
                 </Row>                
