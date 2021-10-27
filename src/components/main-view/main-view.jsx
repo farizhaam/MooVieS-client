58a069b2929fddbdf587dba288816d9fffd8090d
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-27 17:19:19 +0200

    importing GenreView and DirectorView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 import './main-view.scss';
 
+import {GenreView} from '../genre-view/genre-view';
+import {DirectorView} from '../director-view/director-view';
 import {LoginView} from '../login-view/login-view';
 import {RegistrationView} from '../registration-view/registration-view';
 import {MovieCard} from '../movie-card/movie-card';
@@ -86,7 +90,9 @@ export class MainView extends React.Component {
         return (
             <div>
                 {/* logout button */}
-                <button className="logout" onClick={() => {this.onLoggedOut()}}>Logout</button>
+
+                <button className="logout-button" onClick={() => {onLoggedOut(null);}}>Logout</button>
+
 
                 {/* adding MovieCard and MovieView inside Router */}
                 <Router> 
