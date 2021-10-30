33601b62c0e21dd4897412b6155313723903084f
Farizha Aulia Martakusuma <farizha.aulia@gmail.com>
2021-10-30 22:22:33 +0200

    implementing <Provider>, calling moviesApp and devToolsEnhancer() in const store

--- src/index.jsx
+++ src/index.jsx
@@ -4,20 +4,26 @@ import ReactDOM from 'react-dom';
 
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {devToolsEnhancer} from 'redux-devtools-extension';
 import moviesApp from './reducers/reducers';
 
-import MainView from './components/main-view/main-view';
+import {MainView} from './components/main-view/main-view';
 
 //import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp, devToolsEnhancer());
 
 //main component
 class MooViesApplication extends React.Component {
     render() {
         return (
-            <Container>
-                <MainView />   
-            </Container>
+            <Provider store={store}>
+                <Container>
+                    <MainView />   
+                </Container>            
+            </Provider>
+
         ); 
     }
 }
